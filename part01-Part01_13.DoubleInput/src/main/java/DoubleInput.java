e004002a5da07842c182add54cca800defa20dd8
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:21:19 +0000

    Update DoubleInput.java

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -6,7 +6,9 @@ public class DoubleInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
         System.out.println("Give a number: ");
+        // reads user input on nextline, converts from string to double, and assigns to value
         double value = Double.valueOf(scanner.nextLine());
         System.out.println("You gave the number " + value);
 
