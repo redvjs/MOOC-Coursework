70f6e06f3df9d1c029f065cc48d4f5eac9b12d37
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:14:52 +0000

    Update MessageThreeTimes.java

--- part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class MessageThreeTimes {
 
     public static void main(String[] args) {
+        // initialize scanner
         Scanner scanner = new Scanner(System.in);
-
+        // print instructions
         System.out.println("Write a message:");
-        // Write your program here
+        // assigns String variable message to input on the next line
         String message = scanner.nextLine();
-        
+        // prints the message three times
         System.out.println(message);
         System.out.println(message);
         System.out.println(message);
