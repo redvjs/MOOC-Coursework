75ae8db6e30b2f887eff770dfa8a2dea861c10a1
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:23:25 +0000

    Update DifferentTypesOfInput.java

--- part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -5,16 +5,16 @@ public class DifferentTypesOfInput {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        // assigns nextline input to string
         System.out.println("Give a string:  ");
         String string = scan.nextLine();
-        
+        // assigns nextline input to integer, and converts to int
         System.out.println("Give an integer: ");
         int integer = Integer.valueOf(scan.nextLine());
-        
+        // assigns nextline input to floatPoint, and converts to double
         System.out.println("Give a double: ");
         double floatPoint = Double.valueOf(scan.nextLine());
-        
+        // assigns nextline input to TF, and converts to boolean
         System.out.println("Give a boolean: ");
         boolean TF = Boolean.valueOf(scan.nextLine());
         
