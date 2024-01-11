b37654e402c78d869359db26a1e6e74e3afba368
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:11:24 +0000

    Update Message.java

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -5,12 +5,13 @@ public class Message {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        // Prints "Write a message:"
         System.out.println("Write a message:");
-        // Write your program here
         
+        // Takes User input and saves it in the variable *message*
         String message = scanner.nextLine();
-        
+
+        // Prints string variable *message* 
         System.out.println(message);
 
     }
