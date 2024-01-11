c22357c33dd2e862e60197727c03046b0fe4e74a
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:07:46 +0000

    Update Sandbox.java

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -2,7 +2,7 @@
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here4
+        // Prints "Hello World"
         System.out.println("Hello World");
     }
 }
