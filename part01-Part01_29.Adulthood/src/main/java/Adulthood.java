3837d1c4965e3894b4c2a5dcc0f1d0bc3683593e
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:36:04 +0000

    Update Adulthood.java

--- part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -7,8 +7,10 @@ public class Adulthood {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("How old are you? ");
+        // check if input greater than or equal to 18, if true print
         if (Integer.valueOf(scan.nextLine()) >= 18) {
             System.out.println("You are an adult");
+        // if first if statement is false, print
         } else {
             System.out.println("You are not an adult");
         }
