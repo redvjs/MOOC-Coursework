9277e1687eb878c17b77c3fedf2bcb5da47e1e35
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:08:47 +0000

    Update OnceUponATime.java

--- part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
+++ part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -2,7 +2,7 @@
 public class OnceUponATime {
 
     public static void main(String[] args) {
-        // Write your program here
+        // Prints line by line 
         System.out.println("Once upon a time");
         System.out.println("there was");
         System.out.println("a program");
