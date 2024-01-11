6dc89df75390da2a9b63d212d3376c30d48ec90f
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:36:49 +0000

    Update LargerThanOrEqualTo.java

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -9,7 +9,7 @@ public class LargerThanOrEqualTo {
         int first = Integer.valueOf(scan.nextLine());
         System.out.println("Give the second number: ");
         int second = Integer.valueOf(scan.nextLine());
-        
+        // compare first and second values, print if true
         if (first > second) {
             System.out.println("Greater number is: " + first);
         } else if (second > first) {
