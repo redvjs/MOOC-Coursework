043fc212441dc67a5e1ad088f512a0f91bc04748
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:13:15 +0000

    Update HiAdaLovelace.java

--- part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -2,7 +2,9 @@
 public class HiAdaLovelace {
 
     public static void main(String[] args) {
+        // assigns "Ada Lovelace" to the String variable *name*
         String name = "Ada Lovelace";
+        // prints "Hi Ada Lovelace!" 
         System.out.println("Hi " + name + "!");
     }
 }
