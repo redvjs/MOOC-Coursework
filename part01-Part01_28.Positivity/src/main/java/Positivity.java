fea621f409ea2c7ec176d699cc34b8dcb1f477d4
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:35:10 +0000

    Update Positivity.java

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -7,8 +7,10 @@ public class Positivity {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Give a number: ");
+        // check if input value is greater than 0, if true print
         if (Integer.valueOf(scan.nextLine()) > 0) {
             System.out.println("The number is positive.");
+        // if first check false, print
         } else {
             System.out.println("The number is not positive.");
         }
