5a1ac28ff613361c73e6d67fb1ccd0d23d60c28b
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:17:34 +0000

    Update Story.java

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -6,12 +6,16 @@ public class Story {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        // prints text
         System.out.println("I will tell you a story, but I need some information first.");
         System.out.println("What is the main character called?");
+        // assigns user input to variable
         String name = scanner.nextLine();
+        // prints text
         System.out.println("What is their job?");
+        // assigns user input
         String job = scanner.nextLine();
+        // prints story using user input to fill in the blanks.
         System.out.println("Here is the story:");
         System.out.println("Once upon a time there was " + name + ", who was " + job + ".");
         System.out.println("On the way to work, " + name + " reflected on life.");
