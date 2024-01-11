9fcb1200eaf5d3120dfe94af7bc195ded85b87ac
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:16:41 +0000

    Update Conversation.java

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -6,11 +6,15 @@ public class Conversation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        // print text
         System.out.println("Greetings! How are you doing?");
+        // assign user input
         String one = scanner.nextLine();
+        // print text 
         System.out.println("Oh, how interesting. Tell me more!");
+        // assign user input
         String two = scanner.nextLine();
+        // print text
         System.out.println("Thanks for sharing!");
     }
 }
