d65123533a3f829dbeedb7a2037f3ac78e964afb
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:40:47 +0000

    Update Password.java

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -5,7 +5,14 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here 
+        
+        System.out.println("Password?");
+        String first = scan.nextLine();
+        // compare strings using .equals to see if they are the same
+        if (first.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
