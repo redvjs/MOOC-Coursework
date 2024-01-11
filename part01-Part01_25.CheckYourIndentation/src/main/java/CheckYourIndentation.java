06191992a0bf0d67418cbbe7bf1e782d3a99c524
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:33:17 +0000

    Update CheckYourIndentation.java

--- part01-Part01_25.CheckYourIndentation/src/main/java/CheckYourIndentation.java
+++ part01-Part01_25.CheckYourIndentation/src/main/java/CheckYourIndentation.java
@@ -9,10 +9,13 @@ public class CheckYourIndentation {
         int first = Integer.valueOf(scan.nextLine());
         System.out.println("Give another number: ");
         int second = Integer.valueOf(scan.nextLine());
+        // if two values are equal, print "same"
         if (first == second) {
             System.out.println("Same!");
+        // if not equal, check if first value is greater, if true print 1 > 2    
         } else if (first > second) {
             System.out.println("The first was larger than the second!");
+        // else if neither of these checks true, print second is larger.
         } else {
             System.out.println("The second was larger than the first!");
         }
