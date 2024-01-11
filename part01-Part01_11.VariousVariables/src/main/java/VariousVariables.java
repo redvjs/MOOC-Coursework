91c610756b61c912ae8c1340f663e49c5c6dd501
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:19:15 +0000

    Update VariousVariables.java

--- part01-Part01_11.VariousVariables/src/main/java/VariousVariables.java
+++ part01-Part01_11.VariousVariables/src/main/java/VariousVariables.java
@@ -3,21 +3,24 @@ public class VariousVariables {
 
     public static void main(String[] args) {
         // MODIFY THESE:
-
-        int numberOfChicken = 9000;
+        // assigning integer to int variable
+        int numberOfChickens = 9000;
+        // assigning floating point number to double variable
         double baconWeight = 0.1;
+        // assigning string to String variable
         String tractor = "Zetor";
 
         // DON'T MODIFY THESE:
+        // printing
         System.out.println("Chicken:");
-        System.out.println(numberOfChicken);
+        System.out.println(numberOfChickens);
         System.out.println("Bacon (kg):");
         System.out.println(baconWeight);
         System.out.println("Tractor:");
         System.out.println(tractor);
         System.out.println("");
         System.out.println("And finally, a summary:");
-        System.out.println(numberOfChicken);
+        System.out.println(numberOfChickens);
         System.out.println(baconWeight);
         System.out.println(tractor);
     }
