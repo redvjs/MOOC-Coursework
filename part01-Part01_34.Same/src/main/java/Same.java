2a318a91b76f7cef29ca337f1deca60c0a2eb60b
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:41:18 +0000

    Update Same.java

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,15 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Enter first string:");
+        String first = scan.nextLine();
+        System.out.println("Enter second string:");
+        String second = scan.nextLine();
+        // compare strings to see if they are the same
+        if (first.equals(second)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
