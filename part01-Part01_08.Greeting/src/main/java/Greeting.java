bfb226a3a96a0182d4260dfda80a5ea0c943c651
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:15:55 +0000

    Update Greeting.java

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class Greeting {
 
     public static void main(String[] args) {
+        // initialize scanner
         Scanner scanner = new Scanner(System.in);
-        
+        // print instructions
         System.out.println("What's your name? ");
-        
+        // assign variable to user input
         String message = scanner.nextLine();
-        
+        // print "Hi" and the input message
         System.out.println("Hi " + message);
-        // Write your program here
+        
 
     }
 }
