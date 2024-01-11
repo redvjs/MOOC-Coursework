2bf9365da4f24739f8405efcf5c5fe81fa8cd039
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:20:41 +0000

    Update IntegerInput.java

--- part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -6,8 +6,11 @@ public class IntegerInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // print instructions
         System.out.println("Give a number: ");
+        // accepts user input and converts the String into type Integer and assigns it to value
         int value = Integer.valueOf(scanner.nextLine());
+        // prints text + the int
         System.out.println("You gave the number "+ value);
 
     }
