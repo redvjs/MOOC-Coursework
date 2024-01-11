efe813b0c1f10460e983de928cbd84778651c829
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:08:13 +0000

    Update AdaLovelace.java

--- part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -2,7 +2,7 @@
 public class AdaLovelace {
 
     public static void main(String[] args) {
-        // Write your program here
+        // Prints "Ada Lovelace"
         System.out.println("Ada Lovelace");
     }
 }
