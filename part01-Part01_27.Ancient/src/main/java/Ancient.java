e8669beb4b3739d866457e6c72657b1715b3a964
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:34:33 +0000

    Update Ancient.java

--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -7,6 +7,7 @@ public class Ancient {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Give a year: ");
+        // check if user input int is less than 2015, if true print 
         if (Integer.valueOf(scan.nextLine()) < 2015) {
             System.out.println("Ancient history!");
         }
