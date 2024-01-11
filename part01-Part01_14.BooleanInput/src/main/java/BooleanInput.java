2c759a92ebfc36c6158c0675bdd35ee891bf8a57
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:21:49 +0000

    Update BooleanInput.java

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -7,6 +7,7 @@ public class BooleanInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write something: ");
+        // accepts input, converts string to boolean, assigns to value
         boolean value = Boolean.valueOf(scanner.nextLine());
         System.out.println("True or false? " +value);
 
