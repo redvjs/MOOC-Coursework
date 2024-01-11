f7f9a0e086a6fbcd3c4cd46fce7e4070f6ed3acd
redvjs <104998410+redvjs@users.noreply.github.com>
2024-01-11 14:24:46 +0000

    Update SecondsInADay.java

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,8 +7,11 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How many days would you like to convert to seconds? ");
-        int days = Integer.valueOf(scanner.nextLine());
+        // converts user input to type int, and assigns to variable days
+        int days = Integer.valueOf(scanner.nextLine());\
+        // assigns value to secondsInDay
         int secondsInDay = 86400;
+        // print the multiplication of int days times int secondsInDay
         System.out.println(days * secondsInDay);
     }
 }
